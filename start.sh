05804583481b0e2bb0f3ee13baa0fd427d273e6d
Guilherme Souza <guilherme.souza@tfgco.com>
2017-07-26 19:29:51 -0300

    trying to save file on home folder

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 echo -e ${CA_CONN_CRT} > ${HOME}/ca_conn.crt
+cat ${HOME}/ca_conn.crt
 mqtt-monitor --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-username $MQTT_USERNAME --mqtt-password $MQTT_PASSWORD --statsd-host $STATSD_HOST  --statsd-port $STATSD_PORT $OTHER_OPTIONS --ca-file ${HOME}/ca_conn.crt
