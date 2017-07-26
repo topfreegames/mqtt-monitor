b97dee0e037d9514bda0bc2ac4567f1d6b1c3e76
Guilherme Souza <guilherme.souza@tfgco.com>
2017-07-26 19:24:08 -0300

    Using sudo

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo -e ${CA_CONN_CRT} > /etc/ca_conn.crt
+sudo echo -e ${CA_CONN_CRT} > /etc/ca_conn.crt
 mqtt-monitor --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-username $MQTT_USERNAME --mqtt-password $MQTT_PASSWORD --statsd-host $STATSD_HOST  --statsd-port $STATSD_PORT $OTHER_OPTIONS --ca-file /etc/ca_conn.crt
