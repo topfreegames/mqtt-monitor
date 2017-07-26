e0678cec118162a747998f66da40666c1109bd85
Guilherme Souza <guilherme.souza@tfgco.com>
2017-07-26 19:34:24 -0300

    Removing print

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 echo ${CA_CONN_CRT} > ${HOME}/ca_conn.crt
-cat ${HOME}/ca_conn.crt
 mqtt-monitor --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-username $MQTT_USERNAME --mqtt-password $MQTT_PASSWORD --statsd-host $STATSD_HOST  --statsd-port $STATSD_PORT $OTHER_OPTIONS --ca-file ${HOME}/ca_conn.crt
