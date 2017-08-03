2aeb901248d069ceb4420ff7f37061ab465edd46
Guilherme Souza <guilherme.souza@tfgco.com>
2017-08-03 11:52:16 -0300

    Check per server

--- mqtt_monitor/monitor.py
+++ mqtt_monitor/monitor.py
@@ -25,39 +25,68 @@ class Monitor:
             namespace=self.args.namespace,
         )
 
+    def do_request(self, url):
+        logger.info(
+            "requesting - {}".format(url)
+        )
+        return requests.get(
+            url, auth=(
+                self.args.mqtt_api_username,
+                self.args.mqtt_api_password,
+            )
+        )
+
+    def get_cluster_info(self):
+        url = self.get_url("api/nodes")
+        running_servers = []
+        r = self.do_request(url)
+        servers = r.json()
+        for server in servers:
+            if server["cluster_status"] == "Running":
+                server["url"] = "http://{}:18083".format(
+                    server["name"].split("@")[1]
+                )
+                running_servers.append(server)
+                self.check_service(server["name"])
+                continue
+            self.check_service(server["name"], dogstatsd.CRITICAL)
+        return running_servers
+
+    def check_service(self, server, status=None):
+        if not status:
+            status = self.statsd.OK
+        self.statsd.service_check(
+            "mqtt.{}".format(server),
+            status,
+        )
+
     def run(self):
         while True:
             try:
-                for api in API:
-                    url = self.get_url(api)
-                    logger.info(
-                        "requesting - {}".format(url)
-                    )
-                    r = requests.get(
-                        url, auth=(
-                            self.args.mqtt_api_username,
-                            self.args.mqtt_api_password,
-                        )
-                    )
-                    self.send_metrics(r.json())
+                servers = self.get_cluster_info()
+                for server in servers:
+                    for api in API:
+                        url = self.get_url(api, server=server['url'])
+                        r = self.do_request(url)
+                        self.send_metrics(r.json(), server)
             except Exception as e:
                 logger.error(e)
             finally:
                 time.sleep(15)
 
-    def get_url(self, uri):
-        return "{}/{}".format(self.args.mqtt_api_url, uri)
+    def get_url(self, uri, server=None):
+        if not server:
+            server = self.args.mqtt_api_url
+        return "{}/{}".format(server, uri)
 
-    def send_metrics(self, metrics):
+    def send_metrics(self, metrics, server):
         for k, v in metrics.items():
-            metric = self.transform_metric(k)
+            metric = k.replace("/", ".")
             value = float(v)
             logger.info(
                 "sending metrics - {}: {}".format(metric, value)
             )
-            self.statsd.gauge(metric, value)
-
-    def transform_metric(self, metric):
-        return "{}.{}".format(
-            self.args.namespace, metric.replace("/", ".")
-        )
+            self.statsd.gauge(
+                metric, value,
+                tags=["server:{}".format(server)]
+            )
