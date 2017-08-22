04995dffa05dc023ab288f4dcb4caa02cb013757
Guilherme Souza <guilherme.souza@tfgco.com>
2017-08-22 19:50:08 -0300

    Using servers lenght to test

--- mqtt_monitor/monitor.py
+++ mqtt_monitor/monitor.py
@@ -69,14 +69,14 @@ class Monitor:
                 )
             }
             self.check_service(server_item)
-            running_servers.append(server_item)
+            running_servers.append(server_item, len(servers))
         return running_servers
 
-    def check_service(self, server):
+    def check_service(self, server, servers_count):
         status = self.statsd.OK
         url = self.get_url('api/nodes', server=server['url'])
         r = self.do_request(url)
-        if r.status_code != 200 or len(r.json()) == 1:
+        if r.status_code != requests.codes.ok or len(r.json()) < servers_count:
             status = self.statsd.CRITICAL
         self.statsd.service_check(
             "mqtt.broker",
